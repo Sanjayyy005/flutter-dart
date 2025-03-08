e787580d39219dfe8334bfe0d5d92bb6d50db048
Sanjayyy005 <sanjaybk264@gmail.com>
2025-03-09 00:57:18 +0545

    some changes

---
+++ lib/core/firebase_providers/firebase_providers.dart
@@ -0,0 +1,21 @@
+
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'firebase_providers.g.dart';
+
+
+@riverpod
+FirebaseAuth firebaseAuth(Ref ref) {
+  return FirebaseAuth.instance;
+}
+
+
+@riverpod
+FirebaseFirestore firebaseFirestore(Ref ref) {
+  return FirebaseFirestore.instance;
+}
